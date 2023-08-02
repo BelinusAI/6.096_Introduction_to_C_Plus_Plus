038903fc307fad6574f8b4c6eb47244d26cce67e
Belinus <52972482+BelinusAI@users.noreply.github.com>
2023-08-01 20:52:33 -0500

    Create main.cpp

---
+++ assn01/multipleOfNumbers/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+
+    while(1){
+        cin >> N;
+        /* Ternary operator */
+        // N >= 0 && N % 5 == 0 ? cout << N / 5 << "\n" : cout << -1 << "\n";
+
+        /* break */
+        if(N < 0)
+            break;
+
+        /* continue */
+        if(N % 5 > 0)
+            continue;
+
+        cout << N / 5 << "\n";
+    }
+
+    cout << "Goodbye!";
+    return 0;
+}
