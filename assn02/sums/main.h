d2e20908721112581123bdff98917c410bb86c5c
Belinus <52972482+BelinusAI@users.noreply.github.com>
2023-08-02 08:02:26 -0500

    Add files via upload

---
+++ assn02/sums/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int sum(const int, const int, const int, const int);
+int sum(const int [], const int);
+double sum(const double, const double);
+
+#endif // MAIN_H_INCLUDED
