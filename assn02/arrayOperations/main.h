fecbd02287905764cd7b109ece38dd3afa80f512
Belinus <52972482+BelinusAI@users.noreply.github.com>
2023-08-02 10:05:25 -0500

    Add files via upload

---
+++ assn02/arrayOperations/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#define LENGTH  10
+#define WIDTH   10
+
+void printArray(const int [], const int);
+void reverseArray(int [], const int);
+void transpose (const int [][LENGTH], int [][WIDTH]);
+
+#endif // MAIN_H_INCLUDED
